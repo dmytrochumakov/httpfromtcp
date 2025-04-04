ca5528de022137019ae5b20eb6139ba5ea443f34
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2025-04-04 13:58:44 +0300

    feat: reading from file

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
-	print("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println("can't open file")
+		return
+	}
+	defer file.Close()
+
+	res := make([]byte, 8)
+	for {
+		n, err := file.Read(res)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("can't read from file")
+			return
+		}
+		fmt.Printf("read: %s\n", res[:n])
+	}
 }
