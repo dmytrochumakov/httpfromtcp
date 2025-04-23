9d383599c97c63dd275cccfbebb4190eafde30ce
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2025-04-23 13:53:31 +0300

    feat: refactor using channels and goroutine

--- main.go
+++ main.go
@@ -13,30 +13,46 @@ func main() {
 		fmt.Println("can't open file")
 		return
 	}
-	defer file.Close()
 
-	res := make([]byte, 8)
-	var currentLine string
+	messages := getLinesChannel(file)
+	for msg := range messages {
+		fmt.Printf("read: %s\n", msg)
+	}
 
-	for {
-		n, err := file.Read(res)
-		if err != nil {
-			if err == io.EOF {
-				break
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	channelOfStrings := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(channelOfStrings)
+
+		res := make([]byte, 8)
+		var currentLine string
+
+		for {
+			n, err := f.Read(res)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				fmt.Println("can't read from file")
+				return
 			}
-			fmt.Println("can't read from file")
-			return
-		}
-		data := currentLine + string(res[:n])
-		parts := strings.Split(data, "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s\n", parts[i])
+			data := currentLine + string(res[:n])
+			parts := strings.Split(data, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				channelOfStrings <- parts[i]
+			}
+
+			currentLine = parts[len(parts)-1]
 		}
 
-		currentLine = parts[len(parts)-1]
-	}
+		if currentLine != "" {
+			channelOfStrings <- currentLine
+		}
+	}()
 
-	if currentLine != "" {
-		fmt.Println(currentLine)
-	}
+	return channelOfStrings
 }
