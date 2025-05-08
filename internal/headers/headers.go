2495f1539b8b01fed89fd75746e945243770a1e7
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2025-05-08 14:44:41 +0300

    fead: added key header constraints

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -27,11 +27,46 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	}
 
 	key := strings.TrimSpace(string(line[:colonIndex]))
+	value := strings.TrimSpace(string(line[colonIndex+1:]))
+
+	if !keyIsValid(key) {
+		return 0, false, errors.New("invalid key")
+	}
+
 	if key == "" {
 		return 0, false, errors.New("header key is empty")
 	}
-	value := strings.TrimSpace(string(line[colonIndex+1:]))
+
+	key = strings.ToLower(key)
 	h[key] = value
 
 	return lineEnd + 2, false, nil
+}
+
+func keyIsValid(s string) bool {
+	for _, char := range s {
+		if !isAllowedKeyChar(char) {
+			return false
+		}
+	}
+	return true
+}
+
+func isAllowedKeyChar(r rune) bool {
+	if r >= 'A' && r <= 'Z' {
+		return true
+	}
+	if r >= 'a' && r <= 'z' {
+		return true
+	}
+	if r >= '0' && r <= '9' {
+		return true
+	}
+
+	switch r {
+	case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+		return true
+	}
+
+	return false
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -14,7 +14,7 @@ func TestHeaders(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
@@ -32,8 +32,16 @@ func TestHeaders(t *testing.T) {
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 28, n)
+	assert.False(t, done)
+
+	// Test: With Invalid character in header key
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
 	assert.False(t, done)
 }
 
